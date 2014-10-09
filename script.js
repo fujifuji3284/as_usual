c039814eb1b0f97acca6ea4ae2c21cdc50ff3202
fujifuji3284 <fujikunipro@yahoo.co.jp>
2014-10-09 18:12:19 +0900

    te

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ $(function(){
 		var w = $(window).width();
 		$("div#header").css({width:w+"px"});
 		$("div#header").css({height:w/1.4375+"px"});
-		$("div#header > div span").css({fontSize:w/15+"px"});
+		$("div#header > div span").css({fontSize:w/14+"px"});
 	});
 	$("#tuka").css({top:w-250+"px",left:"35%"});
 	$("#u").css({top:w-250+"px",left:"40%"});
 	setTimeout(function(){
 		$("#kara").css({top:"40%",left:"25%",opacity:"0.4"}).fadeIn(5000);
-		$("#mi").animate({top:"-50px",left:"-90px"},600).animate({top:"-70px",left:"50px"},600).animate({top:"30%",left:"15%"},800).css({opacity:"0.4"});
+		$("#mi").animate({top:"-50px",left:"-90px"},600).animate({top:"-70px",left:"50px"},600).animate({top:"30%",left:"14%"},800).css({opacity:"0.4"});
 		$("#ru").animate({top:"-70px",left:"50px"},600).animate({top:"-50px",left:"-90px"},600).animate({top:"30%",left:"20%"},800).css({opacity:"0.4"});
 		$("#sa1").animate({top:"30%",left:"25%"},2000).css({opacity:"0.3"});
 		$("#i1").animate({top:"30%",left:"30%"},2000).css({opacity:"0.3"});
@@ -23,10 +23,10 @@ $(function(){
 		$("#sa2").fadeIn(1000).animate({top:"50%",left:"30%"},2000);
 		$("#i2").fadeIn(2000).animate({top:"50%",left:"35%"},2000);
 		$("#to2").fadeIn(3000).animate({top:"50%",left:"40%"},2000);
-		$("#he").css({top:"50%",left:"45%"}).fadeIn(4000);
+		$("#he").css({top:"50%",left:"46%"}).fadeIn(4000);
 	},4550);
 	setTimeout(function(){
-		$("#tuka").animate({top:"50%",left:"20%"},800);
+		$("#tuka").animate({top:"50%",left:"19%"},800);
 		$("#u").animate({top:"50%",left:"25%"},1000);
 	},4000);
 	setTimeout(function(){
@@ -48,7 +48,7 @@ $(function(){
 		$(this).stop().animate({backgroundColor:"#FF9900"});
 	},
 	function(){
-		$(this).stop().animate({backgroundColor:"#fff"});
+		$(this).stop().animate({backgroundColor:"transparent"});
 	});
 	$(".globNav-inner,.globNav-toggle").css({left:"-1000px"});
 	$(window).scroll(function(){
